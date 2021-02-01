1e38fb76e8daf1ba4c49d804ca5742fa5a7217b3
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2021-01-31 21:30:20 -0800

    changed it so path to lucene is defined outside of compile.sh

--- main/compile.sh
+++ main/compile.sh
@@ -1,4 +1,2 @@
-PATH_TO_LUCENE=/usr/lib/lucene-8.7.0
-
 [ -d ../bin ] || mkdir ../bin
 javac -cp $PATH_TO_LUCENE/core/lucene-core-8.7.0.jar:$PATH_TO_LUCENE/queryparser/lucene-queryparser-8.7.0.jar:$PATH_TO_LUCENE/analysis/common/lucene-analyzers-common-8.7.0.jar:$PATH_TO_LUCENE/demo/lucene-demo-8.7.0.jar -d ../bin ../src/*.java
