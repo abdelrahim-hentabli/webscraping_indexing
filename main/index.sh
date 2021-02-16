c1dca4a6453c3cfdbae3c8649337acd740428056
Abdelrahim Hentabli <abdelrahim.hentabli@gmail.com>
2021-02-15 21:09:11 -0800

    changed compile and run scripts

---
+++ main/index.sh
@@ -0,0 +1,1 @@
+java -cp ../bin:$PATH_TO_LUCENE/core/lucene-core-8.7.0.jar:$PATH_TO_LUCENE/queryparser/lucene-queryparser-8.7.0.jar:$PATH_TO_LUCENE/analysis/common/lucene-analyzers-common-8.7.0.jar:$PATH_TO_LUCENE/demo/lucene-demo-8.7.0.jar:../lib/opencsv-3.8.jar LuceneIndex
